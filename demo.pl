9e680c94fdea60a8da445debceb619fd65ae97fe
Christian Herzog <daduke@daduke.org>
2015-02-24 20:21:51 +0100

    add demo script

---
+++ demo.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+use Robotics::IRobot::Roowifi;
+
+my $robot=Robotics::IRobot::Roowifi->new('192.168.0.11');   #specify RooWifi IP here
+
+$robot->init(); 
+
+$robot->startSafeMode();
+$robot->setMovementCorrectionMode("robot");
+
+$robot->setLCD("ohai");
+
+$robot->forward(100);
+
+$robot->refreshSensors(); 
+
+my $sensorState=$robot->getSensorState();
+
+while($sensorState->{totalDistance}<60) {
+    $robot->refreshSensors();
+}
+$robot->stop();
+
+$robot->rotateRight(20);
+
+$robot->refreshSensors(); 
+
+$sensorState=$robot->getSensorState();
+
+while($sensorState->{totalActualAngle}<1.57) {
+    $robot->refreshSensors();
+}
+
+$robot->drive(100,0);
+
+sleep 1;
+
+$robot->stop();
+$robot->close();
